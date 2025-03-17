fa46aa12c310825063863c5308be77a319ddf366
PlasmOwO <thomas.plasmo@gmail.com>
2025-03-17 12:47:24 +0100

    big update

--- app.py
+++ app.py
@@ -1,22 +1,10 @@
-from flask import Flask,render_template,Flask, render_template, redirect, url_for,request
-from urllib.parse import urlparse
-from urllib.parse import parse_qs
-import sys
+from fastapi import FastAPI
+import unidecode
+import main
 
-from moteur_search import train_search
+model = main.Main()
+app = FastAPI()
 
-
-app = Flask(__name__)
-@app.route("/")
-def index():
-    return render_template("index.html")
-
-@app.route("/search")
-def search() :
-    resultat = train_search(str(request.args.get("fname")))
-    return "resultat : " + str(resultat) 
-
-
-
-if __name__ == "__main__" :
-    app.run(host='0.0.0.0',debug=True)
+@app.post("/testing")
+async def testing(input_string):
+    return model.predict(input_string)
