36b9384998471a9eb491eb1cd53679361cb31ea4
PlasmOwO <thomas.plasmo@gmail.com>
2025-03-18 12:06:57 +0100

    python main

--- Dockerfile
+++ Dockerfile
@@ -2,4 +2,4 @@ FROM python:3.9-slim
 COPY . .
 RUN pip install -r requirements.txt
 EXPOSE 8080
-CMD ["fastapi", "run","app.py"]
+CMD ["python", "app.py"]
--- app.py
+++ app.py
@@ -8,3 +8,7 @@ app = FastAPI()
 @app.post("/testing")
 async def testing(input_string):
     return model.predict(input_string)
+
+if __name__ == "__main__" :
+    import uvicorn
+    uvicorn.run(app, port="8080")
